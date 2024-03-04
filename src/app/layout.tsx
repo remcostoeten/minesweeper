60128ba94d137b506fb361f4b8a6946fd2ce67ab
Remco Stoeten <remcostoeten@hotmail.com>
2024-03-04 21:46:18 +0100

    work on loic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-900 text-white`}>{children}</body>
+      <body className={`${inter.className} bg-slate-900 text-white`}><Toaster/>{children}</body>
     </html>
   );
 }
