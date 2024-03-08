1c2053c94422a6629dcf27eb2ae7122c2b2b2659
Remco Stoeten <remcostoeten@hotmail.com>
2024-03-08 01:19:10 +0100

    btn

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,5 @@
-import * as React from "react"
+// @ts-nocheck
+ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
@@ -41,14 +42,16 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, children, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {children}
+      </Comp>
     )
   }
 )
