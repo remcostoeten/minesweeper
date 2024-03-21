7d3c3f4c2e5369cfe6990f9d450d5f15e143906c
Remco Stoeten <remcostoeten@hotmail.com>
2024-03-21 21:25:21 +0100

    add animated tooltip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import React, { useState } from "react"
 
 import PlaceBetComponent from "@/components/game-logic/b"
 import Minesweeper from "@/components/shells/v1"
+import AnimatedTooltio from "@/components/effects/AnimatedTooltio";
 
 export default function page() {
   const [bet, setBet] = useState(0);
   return (
     <>
     <PlaceBetComponent bet={bet} setBet={setBet} />
-
+    <AnimatedTooltio/>
       <Minesweeper />
     </>
   )
